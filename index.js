230f79fdd74a0b79b66ae2a747f0b8fdfae438cc
jwLiew <devjwliew@gmail.com>
2022-12-30 19:21:38 +0800

    added milliseconds

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ let intervalId;
 let hrs = 0;
 let mins = 0;
 let secs = 0;
+let millisecs = 0;
 
 startBtn.addEventListener("click", () => {
     if(paused){
         paused = false;
         startTime = Date.now() - elapsedTime;
-        intervalId = setInterval(updateTime, 1000);
+        intervalId = setInterval(updateTime, 1);
     }
 });
 
@@ -37,21 +38,24 @@ resetBtn.addEventListener("click", () => {
     hrs = 0;
     mins = 0;
     secs = 0;
-    timeDisplay.textContent = "00:00:00";
+    millisecs = 0;
+    timeDisplay.textContent = "00:00:00:00";
 });
 
 function updateTime(){
     elapsedTime = Date.now() - startTime;
 
+    millisecs = Math.floor((elapsedTime % 1000) / 10);
     secs = Math.floor((elapsedTime / 1000) % 60);
     mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
     hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
 
+    millisecs = pad(millisecs);
     secs = pad(secs);
     mins = pad(mins);
     hrs = pad(hrs);
 
-    timeDisplay.textContent = `${hrs}:${mins}:${secs}`; 
+    timeDisplay.textContent = `${hrs}:${mins}:${secs}:${millisecs}`; 
 
     function pad(unit){
         return (("0") + unit).length > 2 ? unit : "0" + unit;
